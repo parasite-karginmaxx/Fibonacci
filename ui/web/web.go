5aaed320a15a9bbe36088bc415377c93d66dea18
Maxim Kargin <karginmaxx@gmail.com>
2024-05-06 15:24:01 +0300

    отредактировал web.go

--- ui/web/web.go
+++ ui/web/web.go
@@ -10,13 +10,14 @@ import (
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		a := "Введите число:"
-		expression := fmt.Sprintf(a)
+		expression := fmt.Sprint(a)
 		fmt.Fprintf(w, "<h1>%s</h1>", expression)
 		fmt.Fprintf(w, "<form method='post'><input type='text' name='answer'><input type='submit'></form>")
 	} else if r.Method == "POST" {
 		r.ParseForm()
 		answerStr := r.Form.Get("answer")
 		num, err := strconv.Atoi(answerStr)
+
 		if err != nil {
 			http.Error(w, "Invalid answer", http.StatusBadRequest)
 			return
@@ -27,15 +28,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Числа фибоначи рядом: %d, %d\n", prev, curr+prev)
 		} else {
 			prev, curr := logic.Fibonacci(num)
+
 			if num-prev < curr-num {
 				fmt.Fprintf(w, "Ближайшее число Фибоначчи: %d\n", prev)
 			} else {
 				fmt.Fprintf(w, "Ближайшее число Фибоначчи: %d\n", curr)
-
 			}
 		}
 	}
 }
+
 func Main() {
 	port := "8080"
 
